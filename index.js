f551fa291349650b513b7059b574565d99fc9220
nonzamo <nonzamomwende57@gmail.com>
2024-07-14 19:17:14 -0400

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then((json) => console.log(json));
 }
+fetchBooks();
 
 function renderBooks(books) {
   const main = document.querySelector('main');
